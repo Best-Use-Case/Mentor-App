224c735d177de823c0b6818452df2abf05c83e65
Zeray-Kahsay <kahsayzeray@gmail.com>
2025-01-17 21:09:32 +0100

    model classes added

--- API/Models/AppUser.cs
+++ API/Models/AppUser.cs
@@ -14,7 +14,7 @@ public class AppUser
     public required string Gender { get; set; }
     public string FullName
     {
-        get => FirstName + " " + LastName +" "+ UserName;
+        get => FirstName + " " + LastName + " "+ UserName;
     }
 
 
