80b3afbf1673c57e2c865e23656c70bc66132cb6
HenrikFalla <henrik.falla@gmail.com>
2025-01-17 10:42:04 +0100

    Added buttons and styling
    - basic functions for rendering buttons
    - reusable styling in classes for buttons
    - added styling for neutral colour

--- frontend/app/ui/components/buttons.tsx
+++ frontend/app/ui/components/buttons.tsx
@@ -1,15 +1,40 @@
 import Link from "next/link";
 import clsx from "clsx";
 
-export function Button({type = "primary", target, text}: {type?: "primary" | "secondary", target: string, text: string}) {
+export function ButtonLink({type = "primary", target, text}: {type?: "primary" | "secondary", target: string, text: string}) {
     // const primary = type
     const buttonClass = "text-black p-6 pt-3.5 pb-3.5";
-    const wrapperClass = "flex relative bg-primary-300 hover:bg-primary-400 rounded-lg border-solid border-black border-2 after:content-[''] after:absolute after:left-2 after:top-2 after:w-full after:h-full after:block after:rounded-lg after:border-solid after:border-black after:border-2 after:-z-10 after:bg-secondary-300 after:hover:bg-secondary-400";
+    const wrapperClass = clsx (
+        "buttonWrapper",
+        {
+            "buttonWrapperDefault": type == "primary",
+            "buttonWrapperSecondary": type == "secondary",
+        }
+    )
     return (
         <div className={wrapperClass}>
             <Link href={target} className={buttonClass}>
                 {text}
             </Link>
+        </div>
+    )
+}
+export function ButtonFunction({type = "primary", functionName, text}: {type?: "primary" | "secondary", functionName: string, text: string}) {
+    // const primary = type
+    const buttonClass = "text-black p-6 pt-3.5 pb-3.5";
+    const wrapperClass = clsx (
+        "buttonWrapper",
+        {
+            "buttonWrapperDefault": type == "primary",
+            "buttonWrapperSecondary": type == "secondary",
+        }
+    )
+    return (
+        //add onclick run function using the functionName prop
+        <div className={wrapperClass}>
+            <p className={buttonClass}>
+                {text}
+            </p>
         </div>
     )
 }
