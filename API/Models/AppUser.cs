ea2cbb6a0527124151aa633471d86d6b1985565a
Zeray-Kahsay <kahsayzeray@gmail.com>
2025-01-17 21:22:19 +0100

    some user's attributs changed to optional

--- API/Models/AppUser.cs
+++ API/Models/AppUser.cs
@@ -9,12 +9,12 @@ public class AppUser
     public required string UserName { get; set; }
     public required byte[] PasswordHash { get; set; }
     public required byte[] PasswordSalt { get; set; }
-    public required string FirstName { get; set; }
-    public required string LastName { get; set; }
-    public required string Gender { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Gender { get; set; }
     public string FullName
     {
-        get => FirstName + " " + LastName +" "+ UserName;
+        get => FirstName + " " + LastName + " " + UserName;
     }
 
 
