90fb8cd91e65f6ca56345c450afa5615ce402d27
MajidRiazi <majid.riazi@gmail.com>
2025-01-30 09:28:02 +0100

    Sletter filene som er flyttet

--- API/Dtos/RegisterDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API.Dtos;
-
-public record RegisterDto
-{
-    public required string  UserName { get; set; }
-    public required string  Password  { get; set; }
-    public required string  ConfirmPassword  { get; set; } 
-}
