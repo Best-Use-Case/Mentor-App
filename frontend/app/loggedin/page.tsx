7410d3951ddfbb1e33b21ea82b95a2643627d9a9
HenrikFalla <henrik.falla@gmail.com>
2025-02-18 17:33:47 +0100

    Using component to redirect users based on role

--- frontend/app/loggedin/page.tsx
+++ frontend/app/loggedin/page.tsx
@@ -1,35 +1,5 @@
-import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
-interface User {
-	id: string;
-	firstName?: string;
-	lastName?: string;
-	roles?: string;
-	gender?: string;
-	userName?: string;
-}
+import DirectUser from '../(components)/directUser';
 
-export default async function loggedIn() {
-	const session = await getServerSession();
-	if (!session) {
-		redirect('/api/auth/signin');
-	}
-	const user: User = session?.user;
-	const roles = user.roles;
-	switch (roles) {
-		case 'admin':
-			redirect('/loggedin/admin');
-			break;
-		case 'student':
-			redirect('/loggedin/student');
-			break;
-		case 'mentor':
-			redirect('/loggedin/mentor');
-			break;
-		default:
-			redirect('/loggedin/onboarding');
-			break;
-	}
-
-	return;
+export default function loggedIn() {
+	return <DirectUser />;
 }
