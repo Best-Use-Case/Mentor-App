5f167c7467c9d53f5f27ceea63ca130c2332b23d
HenrikFalla <henrik.falla@gmail.com>
2025-02-28 13:42:24 +0100

    Error handling for response
    Need to make sure that the work history does not try to render if the response is empty.

--- frontend/app/(components)/profile/userWorkHistory.tsx
+++ frontend/app/(components)/profile/userWorkHistory.tsx
@@ -19,31 +19,34 @@ export default async function DisplayWorkHistory(props: { userId: string }) {
 	});
 	const res = await response.json();
 	console.log('Work History Response: ', res);
-	if (res.error || res.length === 0) {
+	if (res.error || res.length() === 0) {
 		return null;
-	}
-	return (
-		<section className='flex flex-col gap-2 p-8 rounded-lg bg-neutral-200 dark:bg-neutral-800'>
-			<h2>Work History</h2>
-			<div className='flex flex-col gap-2'>
-				{res.map((item: WorkHistoryItem, key: string) => {
-					return (
-						<div
-							key={key}
-							// className='p-4 border-2 rounded-2xl border-neutral-950 dark:border-neutral-50'
-						>
-							<h5 key={key + 'title'}>{item.jobtitle}</h5>
-							<ul
-								key={key + '-nested-list'}
-								className='list-inside list-disc '
+	} else {
+		return (
+			<section className='flex flex-col gap-2 p-8 rounded-lg bg-neutral-200 dark:bg-neutral-800'>
+				<h2>Work History</h2>
+				<div className='flex flex-col gap-2'>
+					{res.map((item: WorkHistoryItem, key: string) => {
+						return (
+							<div
+								key={key}
+								// className='p-4 border-2 rounded-2xl border-neutral-950 dark:border-neutral-50'
 							>
-								<li key={key + 'list-item-1'}>Company: {item.companyName}</li>
-								<li key={key + 'list-item-2'}>Industry: {item.industryName}</li>
-							</ul>
-						</div>
-					);
-				})}
-			</div>
-		</section>
-	);
+								<h5 key={key + 'title'}>{item.jobtitle}</h5>
+								<ul
+									key={key + '-nested-list'}
+									className='list-inside list-disc '
+								>
+									<li key={key + 'list-item-1'}>Company: {item.companyName}</li>
+									<li key={key + 'list-item-2'}>
+										Industry: {item.industryName}
+									</li>
+								</ul>
+							</div>
+						);
+					})}
+				</div>
+			</section>
+		);
+	}
 }
